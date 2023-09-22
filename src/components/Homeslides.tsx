12912270144e0999d3f812e41ea60bdfb1f1a7f2
tranquyluong <105479930+tranquyluong@users.noreply.github.com>
2023-09-22 13:39:26 +0700

    [FE] layout_website

---
+++ src/components/Homeslides.tsx
@@ -0,0 +1,33 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import { Navigation } from 'swiper/modules';
+
+const Homeslides = () => {
+  return (
+    <div className=''>
+      <Swiper
+        spaceBetween={50}
+        slidesPerView={1}
+        navigation={true}
+        modules={[Navigation]}
+        autoplay={{
+          delay: 3000, // Chuyển slide sau mỗi 3000 mili giây (3 giây)
+        }}
+      >
+        <SwiperSlide>
+          <div className="image">
+            <img className='pt-[100px]' src="https://res.cloudinary.com/dndyxqosg/image/upload/v1695188550/z4710101523983_6628be7f39ed23ea61f8aa79ba0a08ab_up7x1f.jpg" alt="" />
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="image">
+            <img className='pt-[100px]' src="https://res.cloudinary.com/dndyxqosg/image/upload/v1695359501/z4716280626807_a3f2538b67d8595f431405e84fd92d20_c9jsbn.jpg" alt="" />
+          </div>
+        </SwiperSlide>
+      </Swiper>
+    </div>
+  )
+}
+
+export default Homeslides;
