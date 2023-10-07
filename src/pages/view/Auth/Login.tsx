412beceef84e3ff834003950aa01006bfe783138
Tran Hai Duong <haiduong12122003@gmail.com>
2023-10-07 16:28:15 +0700

    style : Create Signin Page

--- src/pages/view/Auth/Login.tsx
+++ src/pages/view/Auth/Login.tsx
@@ -2,7 +2,7 @@ import { BiLogoFacebookCircle } from 'react-icons/bi';
 import { AiOutlineGoogle } from 'react-icons/ai';
 import { Link } from 'react-router-dom';
 
-const Signup = () => {
+const Login = () => {
     return (
         <div className="system-ui bg-gray-300">
             <div className="container mx-auto">
@@ -10,37 +10,11 @@ const Signup = () => {
                     <div className="w-full xl:w-3/4 lg:w-11/12 flex">
                         <div
                             className="w-full h-auto bg-gray-400 hidden lg:block lg:w-6/12 bg-cover rounded-l-lg"
-                            style={{ backgroundImage: 'url("https://www.quadernionline.it/wp-content/uploads/2021/07/siti-web-parma.jpg")' }}
+                            style={{ backgroundImage: 'url("https://images.unsplash.com/photo-1513128034602-7814ccaddd4e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDE4fHx8ZW58MHx8fHx8&w=1000&q=80")' }}
                         ></div>
                         <div className="w-full lg:w-7/12 bg-white p-5 rounded-lg lg:rounded-l-none">
-                            <h3 className="pt-4 text-3xl text-center">Create an Account! 👤</h3>
+                            <h3 className="pt-4 text-3xl text-center">Login 🔑</h3>
                             <form className="px-8 pt-6 pb-8 mb-4 bg-white rounded">
-                                <div className="mb-4 md:flex md:justify-between">
-                                    <div className="mb-4 md:mr-2 md:mb-0">
-                                        <label className="block mb-2 text-sm font-bold text-gray-700" >
-                                            First Name
-                                        </label>
-                                        <input
-                                            className="w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
-                                            id="firstName"
-                                            type="text"
-                                            placeholder="First Name"
-                                            required
-                                        />
-                                    </div>
-                                    <div className="md:ml-2">
-                                        <label className="block mb-2 text-sm font-bold text-gray-700" >
-                                            Last Name
-                                        </label>
-                                        <input
-                                            className="w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
-                                            id="lastName"
-                                            type="text"
-                                            placeholder="Last Name"
-                                            required
-                                        />
-                                    </div>
-                                </div>
                                 <div className="mb-4">
                                     <label className="block mb-2 text-sm font-bold text-gray-700" >
                                         Email
@@ -53,40 +27,36 @@ const Signup = () => {
                                         required
                                     />
                                 </div>
-                                <div className="mb-4 md:flex md:justify-between">
-                                    <div className="mb-4 md:mr-2 md:mb-0">
-                                        <label className="block mb-2 text-sm font-bold text-gray-700" >
-                                            Password
-                                        </label>
-                                        <input
-                                            className="w-full px-3 py-2 mb-3 text-sm leading-tight text-gray-700 border border-red-500 rounded shadow appearance-none focus:outline-none focus:shadow-outline"
-                                            id="password"
-                                            type="password"
-                                            placeholder="******************"
-                                            required
-                                        />
-                                    </div>
-                                    <div className="md:ml-2">
-                                        <label className="block mb-2 text-sm font-bold text-gray-700">
-                                            Confirm Password
-                                        </label>
-                                        <input
-                                            className="w-full px-3 py-2 mb-3 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
-                                            id="c_password"
-                                            type="password"
-                                            placeholder="******************"
-                                            required
-                                        />
-                                    </div>
+                                <div className="mb-4">
+                                    <label className="block mb-2 text-sm font-bold text-gray-700" >
+                                        Password
+                                    </label>
+                                    <input
+                                        className="w-full px-3 py-2 mb-3 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+                                        id="password"
+                                        type="password"
+                                        placeholder="******************"
+                                        required
+                                    />
                                 </div>
+
                                 <div className="mb-6 text-center">
                                     <button
                                         className="w-full px-4 py-2 font-bold text-white bg-orange-500 rounded-full hover:bg-orange-600 focus:outline-none focus:shadow-outline"
                                         type="submit"
                                     >
-                                        Register Account
+                                        Login Now!
                                     </button>
                                 </div>
+                                <div className="text-left">
+                                    <Link to="/forgotpassword">
+                                        <a
+                                            className="inline-block text-sm text-blue-700 align-baseline no-underline">
+                                            Forgot Password?
+                                        </a>
+                                    </Link>
+                                </div>
+
                                 <div className="mb-6 text-center relative">
                                     <div className="flex justify-between items-center">
                                         <hr className="border-t w-1/3" />
@@ -108,12 +78,12 @@ const Signup = () => {
                                     </button>
                                 </div>
                                 <div className="text-center">
-                                    <span> Already have an account? </span>
-                                    <Link to="/login">
+                                    <span>You just heard about Casa? </span>
+                                    <Link to="/signup">
                                         <a
                                             className="inline-block text-sm text-blue-500 align-baseline hover:text-blue-800 no-underline"
                                         >
-                                            Login!
+                                            Signup!
                                         </a>
                                     </Link>
                                 </div>
@@ -126,4 +96,4 @@ const Signup = () => {
         </div>
     )
 }
-export default Signup
+export default Login;
