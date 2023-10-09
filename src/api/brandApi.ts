c587f933a0a247b5eeb505a73478b1252e182a57
Duyy2707 <duy27072003@gmail.com>
2023-10-09 21:30:49 +0700

    feat: create services brands

---
+++ src/api/brandApi.ts
@@ -0,0 +1,57 @@
+
+import { IBrand } from '@/interfaces/brand';
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+
+const brandApi = createApi({
+    reducerPath: 'brands',
+    tagTypes: ['Brands'],
+    baseQuery: fetchBaseQuery({
+        baseUrl: import.meta.env.VITE_API_URL,
+        prepareHeaders: (headers) => {
+            const accessToken = JSON.parse(localStorage.getItem('accessToken')!);
+            if (accessToken) {
+                headers.set('Authorization', `Bearer ${accessToken}`);
+            }
+            return headers;
+        },
+
+    }),
+    endpoints: (builder) => ({
+        getBrand: builder.query<IBrand[], void>({
+            query: () => `/brands`,
+            providesTags: ['Brands']
+        }),
+        getBrandById: builder.query<IBrand, number | string>({
+            query: (id) => `/brands/${id}`,
+            providesTags: ['Brands']
+        }),
+        addBrand: builder.mutation({
+            query: (brand: IBrand)=> ({
+                url: '/brands',
+                method: "POST",
+                body: brand
+            }),
+            invalidatesTags:['Brands']
+        }),
+        removeBrand: builder.mutation({
+            query: (id) =>({
+                url: `/brands/${id}`,
+                method: "DELETE",
+            }),
+            invalidatesTags: ['Brands']
+        }),
+        updateBrand: builder.mutation({
+            query: (brand: IBrand)=> ({
+                url: `/brands/${brand.id}`,
+                method: "PATCH",
+                body: brand
+            }),
+            invalidatesTags:['Brands']
+        })
+    })
+});
+
+export const {useGetBrandQuery, useGetBrandByIdQuery, useAddBrandMutation, useRemoveBrandMutation, useUpdateBrandMutation} = brandApi;
+export const brandReducer = brandApi.reducer;
+export default brandApi
