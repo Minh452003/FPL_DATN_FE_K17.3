093f0905d4746d663505ddc924cbf0131583e23c
Duyy2707 <duy27072003@gmail.com>
2023-10-14 19:04:00 +0700

    feat: ceated services status

---
+++ src/api/statusApi.ts
@@ -0,0 +1,47 @@
+import { IStatus } from '@/interfaces/status';
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+
+const statusApi = createApi({
+    reducerPath: 'status',
+    tagTypes: ['Status'],
+    baseQuery: fetchBaseQuery({
+        baseUrl: import.meta.env.VITE_API_URL,
+        prepareHeaders: (headers) => {
+            const accessToken = JSON.parse(localStorage.getItem('accessToken')!);
+            if (accessToken) {
+                headers.set('Authorization', `Bearer ${accessToken}`);
+            }
+            return headers;
+        },
+
+    }),
+    endpoints: (builder) => ({
+        getStatus: builder.query<IStatus[], void>({
+            query: () => `/status`,
+            providesTags: ['Status']
+        }),
+        addStatus: builder.mutation({
+            query: (status: IStatus)=> ({
+                url: '/status',
+                method: "POST",
+                body: status
+            }),
+            invalidatesTags:['Status']
+        }),
+        updateStatus: builder.mutation({
+            query: (status: IStatus)=> ({
+                url: `/status/${status.id}`,
+                method: "PATCH",
+                body: status
+            }),
+            invalidatesTags:['Status']
+        })
+    })
+});
+
+export const {
+    useGetStatusQuery, useAddStatusMutation, useUpdateStatusMutation
+} = statusApi;
+export const statusReducer = statusApi.reducer;
+export default statusApi
