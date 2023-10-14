547306ae48ebe3507fa8f04afb53c5391d660fbe
Your Name <chubachduong582003@gmail.com>
2023-10-14 19:57:00 +0700

    feat: Show data and delete category

--- src/pages/admin/category/Categorylist.tsx
+++ src/pages/admin/category/Categorylist.tsx
@@ -1,25 +1,32 @@
-import { Image, Table, Button, } from 'antd';
-import { FaTrashCan, FaWrench, FaCirclePlus, FaTrash } from "react-icons/fa6";
+import { useGetCategoryQuery, useRemoveCategoryMutation } from '@/api/categoryApi';
+import { Image, Table, Button,Popconfirm,message } from 'antd';
+import {FaCirclePlus, FaTrash, FaTrashCan, FaWrench } from "react-icons/fa6";
 import { Link } from 'react-router-dom';
+
+
 const Categorylist = () => {
-  const dataSource = [
-    {
-        key: '1',
-        name: 'Sách giáo khoa',
-        stake: '20%'
-    },
-    {
-        key: '2',
-        name: 'Sách Y học',
-        stake: '30%'
-    },
-];
+  const { data} = useGetCategoryQuery();
+  const categories = data?.category.docs;
+  const [removeCategory] = useRemoveCategoryMutation()
+  const [messageApi, contextHolder] = message.useMessage();
+    
+
+
+  const data1 = categories?.map((category: any) => {
+    return {
+        key: category._id,
+        name: category.category_name,
+        stake: category.price_increase_percent,
+        image :<img width={50} src={category.category_image?.url} alt="" />
+    }
+});
+console.log(data1);
+
 const columns = [
     {
         title: 'Ảnh ',
         dataIndex: 'image',
         key: 'image',
-        render: () => <Image src={"https://picsum.photos/200"} width={100} />
     },
     {
         title: 'Danh Mục',
@@ -33,12 +40,32 @@ const columns = [
   }, 
     {
         title: 'Chức năng',
-        render: () => (
-            <div >
-                <Button  className='mr-5 text-blue-500' ><Link to={'edit/:id'}><FaWrench/></Link></Button>
-                <Button className='text-red-500'><Link to={'remove/:id'}><FaTrashCan/></Link></Button> 
-            </div>
-        )
+        render: ({key:_id}:{key:number|string}) => (
+          <div>
+                <Popconfirm
+                    title="Xóa sản phẩm"
+                    description="Mày có chắc cmn chắn muốn xóa không??"
+                    onConfirm={() => {
+                      removeCategory(_id)
+                            .unwrap()
+                            .then(() => {
+                                messageApi.open({
+                                    type: "success",
+                                    content: "Xóa sản phẩm thành công",
+                                });
+                            });
+                    }}
+                    okText="Có"
+                    cancelText="Không"
+                >
+                <Button className='text-red-500'><FaTrashCan/></Button>
+                </Popconfirm>
+
+                  <Button  className='mr-5 text-blue-500' ><Link to={'edit/:id'}><FaWrench/></Link></Button>
+
+          </div>
+      ),
+
     }
 
 ];
@@ -48,7 +75,7 @@ const columns = [
     <Button className='m-2 text-3xl text-blue-500'><Link to={'add'}><FaCirclePlus style={{ fontSize: '24', display: 'block' }}/></Link></Button>
     <Button className='m-2  float-right'><Link to={''}><FaTrash style={{ fontSize: '20', display: 'block' }}/></Link></Button>
     
-    <Table dataSource={dataSource} columns={columns}  />
+    <Table dataSource={data1} columns={columns}  />
     </div>
   )
 }
