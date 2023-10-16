727a16dc2643d249f28cab44232afd0fc492b85d
tranquyluong <105479930+tranquyluong@users.noreply.github.com>
2023-10-17 03:48:40 +0700

    [FE] feat/Show data and User

--- src/pages/admin/user/Userlist.tsx
+++ src/pages/admin/user/Userlist.tsx
@@ -1,87 +1,80 @@
-import React from 'react';
-import { Space, Table } from 'antd';
 
-interface DataType {
-  key: string;
-  name: string;
-  image: string;
-  email: string;
-  phone: string;
-  tags: string[];
-}
+import { Table,Image} from 'antd';
+import { useGetUsersQuery } from '@/api/authApi';
 
-const columns = [
-  {
-    title: 'STT',
-    dataIndex: 'key',
-    key: 'key',
-  },
-  {
-    title: 'Name',
-    dataIndex: 'name',
-    key: 'name',
-    render: (text:any) => <a>{text}</a>,
-  },
-  {
-    title: 'Ảnh',
-    dataIndex: 'image', // Corrected dataIndex
-    key: 'image', // Corrected key
-    render: (image:any) => <img src={image} alt="User Avatar" width={100} />,
-  },
-  {
-    title: 'Email',
-    dataIndex: 'email',
-    key: 'email',
-    render: (email:any) => <a>{email}</a>,
-  },
-  {
-    title: 'Số điện thoại',
-    dataIndex: 'phone',
-    key: 'phone',
-    render:(phone:any) => <a>{phone}</a>,
-  },    
-
-  {
-    title: 'Action',
-    key: 'action',
-    render: (record:any) => (
-      <Space size="middle">
-        <a className='ml-2 inline-block rounded bg-indigo-600 px-4 py-2 text-xs font-medium text-white hover:bg-indigo-700'>Update</a>
-      </Space>
-    ),
-  },
-];
 
 const Userlist = () => {
-  const data: DataType[] = [
+  const { data} = useGetUsersQuery();
+  console.log(data);
+  const user = data?.data;
+  const data1 = user?.map((user: any,index :number) => {
+    return {
+        key: user._id,
+        STT: index + 1,
+        name: user.first_name,
+        email: user.email,
+        phone:user.phone,
+        address:user.address,
+        image :  <Image
+        width={100}
+        height={100}
+        src={user.avatar.url}
+      />
+    }
+  });
+  const columns = [
     {
-      key: '1',
-      name: 'John Brown',
-      image: 'https://res.cloudinary.com/dndyxqosg/image/upload/v1676729011/Upload-my-cv/jgcvhqj0fuuf80cehmwr.jpg',
-      email: 'tranquyluong1234@gmail.com',
-      phone: '0325932836',
-      tags: ['nice', 'developer'],
+      title: 'STT',
+      dataIndex: 'STT',
+      key: 'STT',
+      render: (index:any) => <a>{index}</a> ,
+       
     },
     {
-      key: '2',
-      name: 'Jim Green',
-      image: 'https://res.cloudinary.com/dndyxqosg/image/upload/v1676729011/Upload-my-cv/jgcvhqj0fuuf80cehmwr.jpg',
-      email: 'tranquyluong1234@gmail.com',
-      phone: '0325932836',
-      tags: ['loser'],
+      title: 'Name',
+      dataIndex: 'name',
+      key: 'name',
+      render: (text:any) => <a>{text}</a>,
     },
     {
-      key: '3',
-      name: 'Joe Black',
-      image: 'https://res.cloudinary.com/dndyxqosg/image/upload/v1676729011/Upload-my-cv/jgcvhqj0fuuf80cehmwr.jpg',
-      email: 'tranquyluong1234@gmail.com',
-      phone: '0325932836',
-      tags: ['cool', 'teacher'],
+      title: 'Ảnh ',
+      dataIndex: 'image',
+      key: 'image',
+      reder :(record:any) => {
+        return (
+           <Image
+        width={100}
+        height={100}
+        src={record.url}
+           />
+        )
+      }
     },
+    {
+      title: 'Email',
+      dataIndex: 'email',
+      key: 'email',
+      render: (email:any) => <a>{email}</a>,
+    },
+    {
+      title: 'Số điện thoại',
+      dataIndex: 'phone',
+      key: 'phone',
+      render:(phone:any) => <a>{phone}</a>,
+    },    
+    {
+      title: 'Địa chỉ',
+      dataIndex: 'address',
+      key: 'address',
+      render:(address:any) => <a>{address}</a>,
+    }, 
   ];
 
   return (
-    <div><Table columns={columns} dataSource={data} /></div>
+    <div className="container">
+    <h2 className="text-center text-2xl py-2">Trang Quản lý Khách hàng </h2>
+    <Table dataSource={data1} columns={columns}  />
+    </div>
   );
 }
 
