13ad66410daaf993c933ed95ec3cdfc1b7563c88
Thi NguyenDuc <nguyenducthi@gmail.com>
2023-10-18 02:09:41 +0700

    Show-data-delete-products

--- src/pages/admin/products/Productlist.tsx
+++ src/pages/admin/products/Productlist.tsx
@@ -1,34 +1,65 @@
-import { Image, Table, Button, } from 'antd';
+import { useGetProductsQuery, useRemoveForceProductMutation } from '@/api/productApi';
+import { Image, Table, Button, Popconfirm} from 'antd';
 import { FaTrashCan, FaWrench, FaCirclePlus, FaTrash } from "react-icons/fa6";
 import { Link } from 'react-router-dom';
+import { useGetCategoryQuery } from '@/api/categoryApi';
+import { useGetColorsQuery } from '@/api/colorApi';
+import { useGetBrandQuery } from '@/api/brandApi';
+import { useGetmaterialsQuery } from '@/api/materialApi';
+
 const Productlist = () => {
-  const dataSource = [
-    {
-      key: '1',
-      name: 'Bàn Ghê Cafe - Ghê Cefe đầu trâu GG01',
-      category: 'Ghế gỗ Cafe',
-      brand: 'Casa',
-      material: 'Gỗ',
-      color: 'Nâu',
-      price: '2.340.000đ',
-      quantity: '1',
-    },
-    {
-      key: '2',
-      name: 'Sách Y học',
-      category: 'Ghế gỗ Cafe',
-      brand: 'Casa',
-      material: 'Gỗ',
-      color: 'Nâu',
-      price: '2.340.000đ',
-      quantity: '1',
-    },
-  ];
+  const {data} = useGetProductsQuery();
+  const {data:categories} = useGetCategoryQuery<any>();
+  const {data:colors}=useGetColorsQuery<any>();
+  const {data:brands} = useGetBrandQuery<any>();
+  const {data:materials} = useGetmaterialsQuery<any>();
+  const [removeForceProduct] = useRemoveForceProductMutation();
+  
+  
+  
+  
+  
+  const products  = data?.product.docs;
+  const category  = categories?.category?.docs;
+  const color =colors?.color;
+  const brand =brands?.brand;
+  const material=materials?.material;
+
+  
+  
+  
+  const data1 = products?.map((product: any,index:number) => {
+    return {
+      key: product._id,
+      STT: index + 1,
+      name: product.product_name,
+      price:product.product_price,
+      category:product.categoryId,
+      brand:product.brandId,
+      colors:product.colorsId,
+      materials:product.materialId,
+      quantity:product.sold_quantity,
+
+
+      image: <img width={50} src={product.product?.url} alt="" />
+    }
+  });
+ 
+  
+  
+  
   const columns = [
     {
-      title: 'Tên sản phẩm',
+      title: 'STT',
+      dataIndex: 'STT',
+      key: 'STT',
+      render: (index: any) => <a>{index}</a>,
+    },
+    {
+      title: 'Name',
       dataIndex: 'name',
       key: 'name',
+      render: (text: any) => <a>{text}</a>,
     },
     {
       title: 'Ảnh',
@@ -40,21 +71,46 @@ const Productlist = () => {
       title: 'Danh Mục',
       dataIndex: 'category',
       key: 'category',
+      render: (record:any) => {
+        const catename = category?.find((cate:any) => cate._id === record);
+        return catename?.category_name
+        ;
+    }
+
     },
+    
     {
       title: 'Thương hiệu',
       dataIndex: 'brand',
       key: 'brand',
+      render: (record:string) => {
+        const brandname = brand?.find((bra:any) => bra._id === record);
+        return brandname?.brand_name
+        ;
+    }
     },
+   
     {
       title: 'Chất liệu',
-      dataIndex: 'material',
-      key: 'material',
+      dataIndex: 'materials',
+      key: 'materials',
+      render: (record:string) => {
+        const materialname = material?.find((materials:any) => materials._id === record);
+        return materialname?.material_name
+
+        ;
+    }
     },
+
     {
-      title: 'Màu',
-      dataIndex: 'color',
-      key: 'color',
+      title: 'Màu Sắc',
+      dataIndex: 'colors',
+      key: 'colors',
+      render: (record:string) => {
+        const colorname = color?.find((corlors:any) => colors._id === record);
+        return colorname?.colors_name
+        ;
+    }
     },
     {
       title: 'Giá',
@@ -65,13 +121,26 @@ const Productlist = () => {
       title: 'Số lượng',
       dataIndex: 'quantity',
       key: 'quantity',
+      render: (text: any) => <a>{text}</a>,
     },
     {
       title: 'Chức năng',
-      render: () => (
+      render: ({key:_id} :any) => (
         <div >
           <Button className='mr-5 text-blue-500' ><Link to={'edit/:id'}><FaWrench /></Link></Button>
-          <Button className='text-red-500'><Link to={'remove/:id'}><FaTrashCan /></Link></Button>
+          <Popconfirm
+            title="Xóa sản phẩm"
+            description="Mày có chắc cmn chắn muốn xóa không??"
+            onConfirm={() => removeForceProduct(_id)
+                
+            }
+            okText="Có"
+            cancelText="Không"
+          >
+           <Button className='text-red-500'><FaTrashCan /></Button>
+           
+          </Popconfirm>
+          
         </div>
       )
     }
@@ -83,7 +152,7 @@ const Productlist = () => {
       <div className="overflow-x-auto drop-shadow-xl rounded-lg">
         <Button className='m-2 text-3xl text-blue-500'><Link to={'add'}><FaCirclePlus style={{ fontSize: '24', display: 'block' }} /></Link></Button>
         <Button className='m-2  float-right'><Link to={''}><FaTrash style={{ fontSize: '20', display: 'block' }} /></Link></Button>
-        <Table dataSource={dataSource} columns={columns} />
+        <Table dataSource={data1}  columns={columns} />
       </div>
     </div>
   )
