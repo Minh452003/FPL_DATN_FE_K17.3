dbbae0b06aa93568275d6040d22482f675588e48
Duyy2707 <duy27072003@gmail.com>
2023-10-20 09:05:31 +0700

    feat: show data products to view

--- src/pages/view/ProductPage/ProductPage.tsx
+++ src/pages/view/ProductPage/ProductPage.tsx
@@ -1,8 +1,38 @@
+import "../Home/Homepage.css";
+import "../Home/Responsive_homepage.css";
+import { useGetProductSellQuery } from '@/api/productApi';
 import { Pagination } from '@mui/material';
+import { Skeleton } from 'antd';
+import { Link } from 'react-router-dom';
+import { FaArrowRight } from "react-icons/fa6"
+
 const ProductPage = () => {
+  const { data: products, error, isLoading: isLoadingFetching }: any = useGetProductSellQuery();
+
+ 
+
+  const formatCurrency = (number: number) => {
+      return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+  }
+
+  if (isLoadingFetching) return <Skeleton />;
+  if (error) {
+      if ("data" in error && "status" in error) {
+          return (
+              <div>
+                  {error.status} - {JSON.stringify(error.data)}
+              </div>
+          );
+      }
+  }
   return (
-    <div className="px-6 lg:px-0">
-      <h5><span className="mt-2 p-2 font-bold text-red-500">Trang chủ</span> -- sản phẩm</h5>
+    <div className="px-6 lg:px-0 m-28">
+       <div className="flex items-center pb-10">
+                <div className="float-left"><a href="/" className="font-bold" style={{textDecoration: 'none', color: "orange"}}>Trang Chủ</a></div>
+                <FaArrowRight className="ml-2" />
+                <div className="pl-2">Sản Phẩm</div>
+                
+            </div>
       <div className="mt-2 p-2 text-sm">Bộ lọc sản phẩm</div>
       <div className="mt-2 p-2 text-xs font-bold">giúp bạn tìm sản phẩm nhanh hơn</div>
       <div className="mt-2 p-2 flex ">
@@ -29,71 +59,105 @@ const ProductPage = () => {
         </select>
       </div>
       <div>
-      <div className="flex justify-between "><div className="text-red-500 font-bold text-lg">Tất cả các sản phẩm</div> 
-      <select id="small" className=" p-1 w-[75px] pr-3 mb-6 text-sm text-gray-900 border border-orange-400 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-          <option selected>thứ tự</option>
-          <option value="US">1</option>
-          <option value="CA">2</option>
-          <option value="FR">3</option>
-          <option value="DE">4</option>
-        </select>
+      <div className="new_title lt clear_pd " style={{width: "1255px"}}><h4><a  href="/">Tất cả sản phẩm</a></h4></div>
+    {/* --------------------------- List data products -------------------------- */}
+    <div className="sock_slide slider-items slick_margin slick-initialized slick-slider">
+  {products.length > 0 ? (
+    products.map((product: any, index: any) => (
+      <div
+        key={product._id}
+        className="item slick-slide slick-current slick-active mt-10"
+        tabIndex={-1}
+        role="option"
+        aria-describedby={`slick-slide${index + 10}`}
+        style={{ width: "273px" }}
+        data-slick-index={`${index}`}
+        aria-hidden="false"
+      >
+        <div className="col-item1 ">
+          <div className="sale-label sale-top-right">
+            <span>Hot</span>
+          </div>
+          <div className="item-inner">
+            <div className="product-wrapper">
+              <div className="thumb-wrapper">
+                <Link
+                  to={''}
+                  className="thumb flip"
+                  title={product?.product_name}
+                  tabIndex={0}
+                >
+                  <img
+                    className="lazyload loaded"
+                    src={product.image?.url}
+                    alt={product?.product_name}
+                  />
+                </Link>
+              </div>
+            </div>
+            <div className="item-info">
+              <div className="info-inner">
+                <h3 className="item-title">
+                  {" "}
+                  <Link
+                    to={''}
+                    title={product?.product_name}
+                    tabIndex={0}
+                  >
+                    {product?.product_name}
+                  </Link>{" "}
+                </h3>
+                <div className="item-content">
+                  <div className="item-price">
+                    <div className="price-box">
+                      <span className="regular-price">
+                        {" "}
+                        <span className="price">
+                          {formatCurrency(product.product_price)}₫
+                        </span>{" "}
+                      </span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div className="actions hidden-xs hidden-sm remove_html">
+                <form>
+                  <button
+                    className="button btn-cart btn-more"
+                    title="Chi tiết sản phẩm"
+                    type="button"
+                    tabIndex={0}
+                  >
+                    <Link to={''}>
+                      Chi tiết
+                    </Link>
+                  </button>
+                  <input type="hidden" tabIndex={0} />
+                  <button
+                    className="button btn-cart"
+                    title="Mua hàng"
+                    type="button"
+                    tabIndex={0}
+                  >
+                    <Link to={''}>Mua hàng</Link>
+                  </button>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-      <div className="grid grid-cols-1 xxs:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-      <div>
-        <img
-          className="h-auto max-w-full rounded-lg"
-          src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image.jpg"
-          alt=""
-        />
-        <div className="text-center font-bold justify-center">bàn trang điểm</div>
-        <div className="text-center text-red-500">3.500.000 VNĐ</div>
-      </div>
-      <div>
-        <img
-          className="h-auto max-w-full rounded-lg"
-          src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image.jpg"
-          alt=""
-        />
-        <div className="text-center font-bold justify-center">bàn trang điểm</div>
-        <div className="text-center text-red-500">3.500.000 VNĐ</div>
-      </div>
-      <div>
-        <img
-          className="h-auto max-w-full rounded-lg"
-          src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image.jpg"
-          alt=""
-        />
-        <div className="text-center font-bold justify-center">bàn trang điểm</div>
-        <div className="text-center text-red-500">3.500.000 VNĐ</div>
-      </div>
-      <div>
-        <img
-          className="h-auto max-w-full rounded-lg"
-          src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image.jpg"
-          alt=""
-        />
-        <div className="text-center font-bold justify-center">bàn trang điểm</div>
-        <div className="text-center text-red-500">3.500.000 VNĐ</div>
-      </div>
-      <div>
-        <img
-          className="h-auto max-w-full rounded-lg"
-          src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg"
-          alt=""
-        />
-        <div className="text-center font-bold justify-center">bàn trang điểm</div>
-        <div className="text-center text-red-500">3.500.000 VNĐ</div>
-      </div>
-      <div>
-        <img
-          className="h-auto max-w-full rounded-lg"
-          src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg"
-          alt=""
-        />
-        <div className="text-center font-bold justify-center">bàn trang điểm</div>
-        <div className="text-center text-red-500">3.500.000 VNĐ</div>
-      </div>
-      </div>
+    ))
+  ) : (
+    <p>..................không có sản phẩm nào.</p>
+  )}
+</div>
+
+
+    {/* --------------------------- List data products -------------------------- */}
+
+
       </div>
     <div className='flex w-full py-4 justify-center '>
     <Pagination count={4} variant="outlined" shape="rounded" />
