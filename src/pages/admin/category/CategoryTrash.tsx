ca69bd9ed7b339dae54eac81af2d5a8fb10835ce
Your Name <chubachduong582003@gmail.com>
2023-10-22 20:20:19 +0700

    feat: Delete and Restore Product

--- src/pages/admin/category/CategoryTrash.tsx
+++ src/pages/admin/category/CategoryTrash.tsx
@@ -1,8 +1,8 @@
-import { useGetAllDeleteQuery, useRestoreCategoryMutation } from '@/api/categoryApi';
+import { useGetAllDeleteQuery, useRemoveForceCategoryMutation, useRestoreCategoryMutation } from '@/api/categoryApi';
 import { Table, Button } from 'antd';
 import { AiOutlineLoading3Quarters } from 'react-icons/ai';
 import { BiFoodMenu } from 'react-icons/bi';
-import { FaWindowRestore } from "react-icons/fa6";
+import { FaTrashCan, FaWindowRestore } from "react-icons/fa6";
 import { Link } from 'react-router-dom';
 import Swal from 'sweetalert2';
 
@@ -10,9 +10,38 @@ import Swal from 'sweetalert2';
 const CategoryTrash = () => {
   const { data }: any = useGetAllDeleteQuery();
   const categories = data?.category;
+  const [removeCategory, { isLoading: isRemoveLoading }] = useRemoveForceCategoryMutation();
+  const [restoreCategory, { isLoading: isRestoreLoading }] = useRestoreCategoryMutation()
 
-  const [restoreCategory, { isLoading: isRemoveLoading }] = useRestoreCategoryMutation()
-
+  const deleteCategory = (id: any) => {
+    Swal.fire({
+      title: 'Bạn chắc chứ?',
+      text: "Khi xóa bạn không thể khôi phục lại!",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Vâng, tôi chắc chắn!',
+      cancelButtonText: 'Huỷ'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        removeCategory(id).unwrap().then(() => {
+          Swal.fire(
+            'Xoá thành công!',
+            'danh mục của bạn đã được xoá.',
+            'success'
+          )
+        })
+      } else if (result.dismiss === Swal.DismissReason.cancel) {
+        // Hiển thị thông báo hủy xóa sản phẩm
+        Swal.fire(
+          'Thất bại',
+          'danh mục xoá thất bại :)',
+          'error'
+        )
+      }
+    })
+  }
   const restoreCategory1 = (id: any) => {
     Swal.fire({
       title: 'Bạn chắc chứ?',
@@ -25,8 +54,6 @@ const CategoryTrash = () => {
       cancelButtonText: 'Huỷ'
     }).then((result) => {
       if (result.isConfirmed) {
-        // Xóa sản phẩm
-
         restoreCategory(id).unwrap().then(() => {
           Swal.fire(
             'khôi thành công!',
@@ -38,7 +65,7 @@ const CategoryTrash = () => {
         // Hiển thị thông báo hủy xóa sản phẩm
         Swal.fire(
           'Thất bại',
-          'Thương hiệu xoá thất bại :)',
+          'danh mục xoá thất bại :)',
           'error'
         )
       }
@@ -73,9 +100,17 @@ const CategoryTrash = () => {
     {
       title: 'Chức năng',
       render: ({ key: _id }: { key: number | string }) => (
+        
         <div>
-          <Button onClick={() => restoreCategory1(_id)} type="primary" danger className="ml-2">
+           <Button className='mr-1 text-red-500' onClick={() => deleteCategory(_id)}>
             {isRemoveLoading ? (
+              <AiOutlineLoading3Quarters className="animate-spin" />
+            ) : (
+              <FaTrashCan />
+            )}
+          </Button>
+          <Button className='mr-1 text-blue-500' onClick={() => restoreCategory1(_id)} >
+            {isRestoreLoading ? (
               <AiOutlineLoading3Quarters className="animate-spin" />
             ) : (
               <FaWindowRestore />
