780778c648d9e7d76df3bd7c85d5f1a760001346
nguyenducthi <nguyenducthi252003@gmail.com>
2023-10-24 23:49:09 +0700

    fix-show-profile

--- src/pages/view/User/Account/Profile.tsx
+++ src/pages/view/User/Account/Profile.tsx
@@ -1,5 +1,37 @@
+import { useGetUserByIdQuery } from "@/api/authApi";
+import { getDecodedAccessToken } from "@/decoder";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { PiUserBold } from "react-icons/pi";
+// Đây là hàm gửi yêu cầu lấy thông tin người dùng từ máy chủ dựa trên token
+
 const Profile = () => {
+  const [userId, setUserId] = useState(0);
+  const navigate = useNavigate();
+
+  const { data: user } = useGetUserByIdQuery(userId);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        // Gửi yêu cầu lấy thông tin người dùng từ máy chủ dựa trên token
+        const response = await getDecodedAccessToken(); // Assumed function to get user info by token
+        setUserId((response as { id: number }).id);
+
+        // Cập nhật state với thông tin người dùng nhận được từ máy chủ
+      } catch (error) {
+        console.error("Lỗi khi lấy thông tin người dùng: ", error);
+        // Xử lý lỗi nếu cần thiết, ví dụ: chuyển hướng đến trang lỗi hoặc hiển thị thông báo lỗi
+        navigate("/error"); // Chuyển hướng đến trang lỗi nếu không thể lấy thông tin người dùng
+      }
+    };
+    fetchUser(); // Gọi hàm fetchUser khi component được tạo ra
+  }, [navigate]); // useEffect sẽ chạy lại khi navigate thay đổi, giúp lấy lại thông tin người dùng khi người dùng đăng xuất và đăng nhập lại
+
+  if (!user) {
+    return <div>Loading...</div>; // Hiển thị thông báo loading khi đang lấy thông tin người dùng từ máy chủ
+  }
+
   return (
     <div className="container">
       <div className="header">
@@ -19,8 +51,9 @@ const Profile = () => {
                   <div className="">
                     <input
                       type="text"
-                      placeholder="duongcb"
+                      placeholder={user.first_name}
                       className="w-full h-9 px-3 py-2 border border-gray-300 rounded-md transition duration-300 hover:ease-in-out"
+                      disabled
                     />
                   </div>
                 </td>
@@ -33,8 +66,9 @@ const Profile = () => {
                   <div className="py-4">
                     <input
                       type="text"
-                      placeholder="Chu Bach Duong"
+                      placeholder={user.last_name}
                       className="w-full h-9 px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500 transition duration-300 ease-in-out"
+                      disabled
                     />
                   </div>
                 </td>
@@ -44,7 +78,7 @@ const Profile = () => {
                   <label className="pl-4 pb-3 py-3">Email</label>
                 </td>
                 <td className="flex">
-                  <div className="py-4 px-3">duongcbph21404@gmail.com</div>
+                  <div className="py-4 px-3">{user.email}</div>
                   <div className="py-4 px-3">
                     <a href="">thay đổi</a>
                   </div>
@@ -58,8 +92,9 @@ const Profile = () => {
                   <div className="py-4">
                     <input
                       type="text"
-                      placeholder="099999999"
+                      placeholder={user.phone}
                       className="w-full h-9 px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500 transition duration-300 ease-in-out"
+                      disabled
                     />
                   </div>
                 </td>
