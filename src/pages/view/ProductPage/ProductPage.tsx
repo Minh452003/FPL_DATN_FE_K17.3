af6a04dd7bf10cadc45ed08bdc68f762400657a2
nguyenloc9999 <locnvph27057@fpt.edu.vn>
2023-11-03 10:12:10 +0700

    feat:Filter_products_by_category_name_and_brand_name

--- src/pages/view/ProductPage/ProductPage.tsx
+++ src/pages/view/ProductPage/ProductPage.tsx
@@ -1,17 +1,31 @@
 import "../Home/Homepage.css";
 import "../Home/Responsive_homepage.css";
-import { useGetProductSellQuery } from '@/api/productApi';
-import { Pagination } from '@mui/material';
-import { Skeleton } from 'antd';
-import { Link } from 'react-router-dom';
-import { FaArrowRight } from "react-icons/fa6"
-import { useState } from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { useGetProductSellQuery } from "@/api/productApi";
+import { useGetCategoryQuery } from "@/api/categoryApi";
+import { Pagination } from "@mui/material";
+import { Skeleton } from "antd";
+import { FaArrowRight } from "react-icons/fa6";
+import { IProduct } from "@/interfaces/product";
+import { useGetBrandQuery } from "@/api/brandApi";
+
 const ProductPage = () => {
-  const { data: products, error, isLoading: isLoadingFetching }: any = useGetProductSellQuery();
+  const {
+    data: products,
+    error,
+    isLoading: isLoadingFetching,
+  } = useGetProductSellQuery();
+  const { data: categories } = useGetCategoryQuery();
+  const { data: brands } = useGetBrandQuery();
+
   const [selectedPriceFilter, setSelectedPriceFilter] = useState("all");
-  const formatCurrency = (number: number) => {
+  const [selectedCategory, setSelectedCategory] = useState("all");
+  const [selectedBrand, setSelectedBrand] = useState("all");
+
+  const formatCurrency = (number: any) => {
     return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
-  }
+  };
 
   if (isLoadingFetching) return <Skeleton />;
   if (error) {
@@ -23,42 +37,97 @@ const ProductPage = () => {
       );
     }
   }
+
   const filteredProducts = products.filter((product: any) => {
+    const categoryMatches =
+      selectedCategory === "all" || product.categoryId === selectedCategory;
+    const brandMatches =
+      selectedBrand === "all" || product.brandId === selectedBrand;
     if (selectedPriceFilter === "all") {
-      return true; // Hiển thị tất cả sản phẩm khi "Tất cả giá" được chọn
+      return categoryMatches && brandMatches;
     } else if (selectedPriceFilter === "100000-1000000") {
-      return product.product_price >= 100000 && product.product_price <= 1000000;
+      return (
+        categoryMatches &&
+        brandMatches &&
+        product.product_price >= 100000 &&
+        product.product_price <= 1000000
+      );
     } else if (selectedPriceFilter === "1000000-5000000") {
-      return product.product_price >= 1000000 && product.product_price <= 5000000;
+      return (
+        categoryMatches &&
+        brandMatches &&
+        product.product_price >= 1000000 &&
+        product.product_price <= 5000000
+      );
     } else if (selectedPriceFilter === "5000000-10000000") {
-      return product.product_price >= 5000000 && product.product_price <= 10000000;
+      return (
+        categoryMatches &&
+        brandMatches &&
+        product.product_price >= 5000000 &&
+        product.product_price <= 10000000
+      );
     } else if (selectedPriceFilter === "10000000+") {
-      return product.product_price >= 10000000;
+      return (
+        categoryMatches && brandMatches && product.product_price >= 10000000
+      );
     }
   });
+  
 
   return (
     <div className="px-6 lg:px-0 m-28">
       <div className="flex items-center pb-10">
-        <div className="float-left"><a href="/" className="font-bold" style={{ textDecoration: 'none', color: "orange" }}>Trang Chủ</a></div>
+        <div className="float-left">
+          <Link
+            to="/"
+            className="font-bold"
+            style={{ textDecoration: "none", color: "orange" }}
+          >
+            Trang Chủ
+          </Link>
+        </div>
         <FaArrowRight className="ml-2" />
         <div className="pl-2">Sản Phẩm</div>
-
       </div>
       <div className="mt-2 p-2 text-sm">Bộ lọc sản phẩm</div>
-      <div className="mt-2 p-2 text-xs font-bold">giúp bạn tìm sản phẩm nhanh hơn</div>
+      <div className="mt-2 p-2 text-xs font-bold">
+        giúp bạn tìm sản phẩm nhanh hơn
+      </div>
       <div className="mt-2 p-2 flex ">
-        <select id="small" className="block mr-4 p-2 mb-6 text-sm text-gray-900 border border-orange-400 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-          <option selected>Nhà Sản Xuất</option>
-          <option value="US">United States</option>
-          <option value="CA">Canada</option>
-          <option value="FR">France</option>
-          <option value="DE">Germany</option>
+        <select
+          id="small"
+          value={selectedCategory}
+          onChange={(e) => setSelectedCategory(e.target.value)}
+          className="block mr-4 p-2.5 mb-6 text-sm text-gray-900 border border-orange-400 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          //...
+        >
+          <option value="all">Tất cả danh mục</option>
+          {categories?.category?.docs.map((category: any) => (
+            <option key={category._id} value={category._id}>
+              {category.category_name}
+            </option>
+          ))}
         </select>
-        <select id="small"
+        <select
+          id="small"
+          value={selectedBrand}
+          onChange={(e) => setSelectedBrand(e.target.value)}
+          className="block mr-4 p-2.5 mb-6 text-sm text-gray-900 border border-orange-400 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          //...
+        >
+          <option value="all">Tất cả thương hiệu</option>
+          {brands?.brand?.map((brand: any) => (
+            <option key={brand._id} value={brand._id}>
+              {brand.brand_name}
+            </option>
+          ))}
+        </select>
+        <select
+          id="small"
           value={selectedPriceFilter}
           onChange={(e) => setSelectedPriceFilter(e.target.value)}
           className="block mr-4 p-2.5 mb-6 text-sm text-gray-900 border border-orange-400 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          //...
         >
           <option value="all">Tất cả giá</option>
           <option value="100000-1000000">100.000-1.000.000</option>
@@ -66,19 +135,12 @@ const ProductPage = () => {
           <option value="5000000-10000000">5.000.000-10.000.000</option>
           <option value="10000000+">10.000.000+</option>
         </select>
-        <select id="small" className="block p-2 mb-6 text-sm  text-gray-900 border border-orange-400 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-          <option selected>Loại</option>
-          <option value="US">United States</option>
-          <option value="CA">Canada</option>
-          <option value="FR">France</option>
-          <option value="DE">Germany</option>
-        </select>
       </div>
       <div>
         <div className="new_title lt clear_pd " style={{ width: "1255px" }}><h4><a href="/">Tất cả sản phẩm</a></h4></div>
-        {/* --------------------------- List data products -------------------------- */}
+       
         <div className="sock_slide slider-items slick_margin slick-initialized slick-slider">
-          {/* filter theo giá sản phẩm  */}
+          
           {filteredProducts.length > 0 ? (
             filteredProducts.map((product: any, index: any) => (
 
@@ -107,7 +169,7 @@ const ProductPage = () => {
                         >
                           <img
                             className="lazyload loaded"
-                            src={product.image?.url}
+                            src={product.image[0]?.url}
                             alt={product?.product_name}
                           />
                         </Link>
@@ -172,16 +234,12 @@ const ProductPage = () => {
           )}
         </div>
 
-
-        {/* --------------------------- List data products -------------------------- */}
-
-
       </div>
-      <div className='flex w-full py-4 justify-center '>
+    
+      <div className="flex w-full py-4 justify-center ">
         <Pagination count={4} variant="outlined" shape="rounded" />
       </div>
     </div>
-
   );
 };
 
